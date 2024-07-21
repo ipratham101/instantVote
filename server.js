e9623ad94b51c56481d2537fb78b698c8fcc888a
ipratham101 <105946829+ipratham101@users.noreply.github.com>
2024-07-21 22:26:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 app.use(cors());
